b63630ed1286b7ba14a70132b8eb5dab7910924f
Amit <zedmit0870@gmail.com>
2020-01-14 00:57:25 +0530

    'gpaybutton'

--- src/components/cart/CartTotals.js
+++ src/components/cart/CartTotals.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import Gpaybutton from './Gpaybutton';
 
-export default function CartTotals({value}) {
+export default function CartTotals({value, history}) {
     const {cartSubTotal,cartTax,cartTotal,clearCart} = value;
     return (
         <React.Fragment>
@@ -32,6 +33,10 @@ export default function CartTotals({value}) {
                             </span>
                             <strong>$ {cartTotal}</strong>
                         </h5>
+                        <Gpaybutton 
+                        total={cartTotal} 
+                        clearCart={clearCart} 
+                        history={history} />
                     </div>
                 </div>
             </div>
